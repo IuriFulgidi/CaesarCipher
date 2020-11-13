9405a58dc27e3a5cde24526ae6d8ed2129e41add
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-13 09:10:47 +0100

    inizio progetto c=(m - offset + Key) % 26) + offset)

---
+++ CaesarCipher/CaesarCipherLiibrary/CaesarBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CaesarCipherLiibrary
+{
+    public abstract class CaesarBase
+    {
+        protected int Key { get; set; }
+        private char Cipher(char ch)
+        {
+            //se il caratte non è una lettera si restituisce così com'è
+            if (!char.IsLetter(ch))
+                return ch;
+            //si crea la variabile che ha il valore il carattere
+            char offset;
+            //se la lettera è maiuscola si parte dall A maiuscola
+            if (char.IsUpper(ch))
+                offset = 'A';
+            //altrimenti dalla a minuscola
+            else
+                offset = 'a';
+            //il resto tra (la lettera in input meno il valore in base a se sia maiuscola o minuscola
+            //più la chiave) diviso 26 più il valore di prima
+            return (char)(((ch - offset + Key) % 26) + offset);
+        }
+    }
+}
---
+++ CaesarCipher/CaesarCodeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaesarCodeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
