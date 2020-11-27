4aee01fa6e9fb3dd94b92706c18d8c9033e30348
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-27 09:24:54 +0100

    creazione del nuovo ramo con applicazione wpf

---
+++ CaesarCipher/CaesarWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaesarWPF
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btn_DoWork_Click(object sender, RoutedEventArgs e)
+        {
+            string input = txt_input.Text;
+            int key = Convert.ToInt32(txt_key.Text);
+            string output = "";
+            Encoder enc = new Encoder(key);
+            Encoder dec = new Decoder(key);
+
+            if (rdb_Encode.IsChecked==true)
+            {
+                foreach (char lettera in input)
+                    output += enc.Encode(lettera);
+            }
+            else
+            {
+                foreach (char lettera in input)
+                    output += dec.Decode(lettera);
+            }
+
+            lbl_output.Content = output;
+        }
+    }
+}
---
+++ CaesarCipher/ThèCaesar/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThèCaesar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_DoWork_Click(object sender, RoutedEventArgs e)
+        {
+            string input = txt_input.text;
+            int key = txt_key.text;
+
+            if(rdb_Encode.IsChecked)
+            {
+
+            }
+        }
+    }
+}
